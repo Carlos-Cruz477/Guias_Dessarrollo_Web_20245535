d54b696420432fb92a974dc9c05d36433ce47a29
Carlos-Cruz477 <20245535@esen.edu.sv>
2025-10-26 18:54:38 -0600

    Guia 4

---
+++ 20245535_Guia5/js/buttons.js
@@ -0,0 +1,24 @@
+function aviso(){
+    alert("Bienvenido al mundo JavaScript");
+
+}
+
+function confirmacion(){
+    let confirmacion = confirm("¿Desea salir de la sesión?")
+
+    alert(`Valor seleccionado: ${confirmacion}`)
+}
+
+function capturarDatos(){
+    let nombre = prompt("¿Cual es su nombre?")
+    let edad = prompt("¿Cual es su edad?", 0)
+
+    alert(`Su nombe es ${nombre} y su edad es ${edad}`)
+}
+
+function dibujarParrafo(){
+    let parrafo = prompt("Escriba la información que desea ver en el parrafo")
+
+    const p = document.querySelector("#idParrafo")
+    p.innerHTML = parrafo
+}
